b84f10a81d441e62138723fa3195251b3361d5e2
isaiah vang <vang.isaiah3@gmail.com>
2023-04-18 17:09:43 -0400

    added in Scanner and began writing the code to get billing address

--- src/main/java/com/iv/AddressBuilder.java
+++ src/main/java/com/iv/AddressBuilder.java
@@ -1,7 +1,24 @@
 package com.iv;
 
+import java.util.Scanner;
+
 public class AddressBuilder {
     public static void main(String[] args){
+        // Prompt user for their billing address
+        // Prompt user for their mailing address
+        // Use StringBuilder to create one variable that hold everything
+
+        Scanner response = new Scanner(System.in);
+
+        System.out.print("What is your first and last name?");
+        String nameRes = response.nextLine();
+
+
+
 
     }
 }
+//    Scanner myScanner = new Scanner(System.in);
+//
+//        System.out.println("What is your name?");
+//                String nameRes = myScanner.nextLine();
